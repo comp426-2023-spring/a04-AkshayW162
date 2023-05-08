4b9b73fb1a00fa59c541aff0bd7e0b88b882241c
Akshay Walavalkar <akshayw@email.unc.edu>
2023-05-08 08:29:54 -0400

    Setup server.js comment skeletons

--- server.js
+++ server.js
@@ -12,3 +12,27 @@ let port = args.port || 5000;
 //App use
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//Get the root endpoint of your app
+
+//Call a nonexistent endpoint
+
+//Play RPS
+
+//Play RPSLS
+
+//Play RPS against an opponent (URLEncoded data body)
+
+//Play RPSLS against an opponent (URLEncoded data body)
+
+//Play RPS against an opponent (JSON data body)
+
+//Play RPSLS against an opponent (JSON data body)
+
+//Play RPS against an opponent (parameter endpoint)
+
+//Play RPSLS against an opponent (parameter endpoint)
+
+//Look at the package.json file
+
+//Get the headers
