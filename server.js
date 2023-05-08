880183eca3b6dce56287fb7947514c58d10f6aef
Akshay Walavalkar <akshayw@email.unc.edu>
2023-05-08 08:27:46 -0400

    Setup server.js file

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-# placehoder text
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import express from 'express';
+import { rps, rpsls } from './lib/rpsls.js';
+
+//Args, app, and port
+let args = minimist(process.argv.slice(2));
+let app = express();
+let port = args.port || 5000;
+
+//App use
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
